27ef55b300f5cb095451f8131cbbd63d25195996
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-11-22 19:35:01 +0300

    completed

--- lib/testing/ipdb_debugging_test.py
+++ lib/testing/ipdb_debugging_test.py
@@ -7,4 +7,7 @@ class TestIpdbDebugging:
     
     def test_adds_two(self):
         '''adds_two() adds 2 to input arg and returns sum.'''
-        assert(plus_two(3) == 5)
+        result = plus_two(3)
+        print(f"Result: {result}")
+        assert result == 5
+
